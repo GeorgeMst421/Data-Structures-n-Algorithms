8162ae7137508e7f921cb4edef7acedea0fbc047
George <moustosgeorge@gmail.com>
2023-01-23 13:57:47 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+#define SIZE 30
+
+
+
+int main(){
+
+    Graph Graph;
+    initGraph(&Graph);
+    char option = 'a';
+
+    while(option != 'q'){
+        printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+        printf("\nYour options are:\n(i)Insert, (d)Delete, (p)Print, (b)BFS, (k)Kruskal, (m)Primm, (q)Quit");
+        printf("\nGive a new choice: ");
+        scanf("%c",&option);
+        fflush(stdin);
+        option = tolower(option);
+        if(option == 'q')
+            printf("\n~~QUITING~~");        
+        if(option == 'i')
+            Main_Insert(&Graph);       
+        if(option == 'p')
+            printGraph(Graph);
+        if(option == 'd' )
+            Main_Delete(&Graph);
+        if(option == 'b' )
+            Main_BFS(&Graph);
+        if(option == 'k')
+            Main_Kruskal(Graph);
+        if(option == 'm')
+            Main_Primm(Graph);
+    }
+
+    return 0;
+}
+
+
